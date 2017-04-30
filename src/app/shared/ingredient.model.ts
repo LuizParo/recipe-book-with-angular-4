0be5d6b4d802bbdd477de328ddb3ed3a1a71f4ca
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-04-30 17:18:54 -0300

    Giving content to some components

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+    constructor(private _name: string, private _amount: number) {}
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public get amount(): number {
+        return this._amount;
+    }
+}
