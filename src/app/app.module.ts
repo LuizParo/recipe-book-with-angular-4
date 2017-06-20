32bbfec78849403b66e97ba979b51e19a67d07a7
Luiz Guilherme Paro <lg_paro@hotmail.com>
2017-06-20 19:49:39 -0300

    Creating the recipe module feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import 'rxjs/Rx';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RecipeModule } from './recipes/recipe.module';
 
 import { AppComponent } from './app.component';
 import { HeaderCompoment } from './header/header.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeService } from './recipes/recipe.service';
-import { RecipesComponent } from './recipes/recipes.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { DropdownDirective } from './shared/dropdown.directive';
@@ -23,6 +17,7 @@ import { DropdownDirective } from './shared/dropdown.directive';
 import { AuthGuard } from './auth/auth-guard.service';
 import { AuthService } from './auth/auth.service';
 import { DataStorageService } from './shared/data-storage.service';
+import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 import { SigninComponent } from './auth/signin/signin.component';
@@ -32,15 +27,9 @@ import { SignupComponent } from './auth/signup/signup.component';
     declarations: [
         AppComponent,
         HeaderCompoment,
-        RecipeDetailComponent,
-        RecipeItemComponent,
-        RecipeListComponent,
-        RecipeStartComponent,
-        RecipesComponent,
         ShoppingListComponent,
         ShoppingEditComponent,
         DropdownDirective,
-        RecipeEditComponent,
         SigninComponent,
         SignupComponent
     ],
@@ -49,7 +38,7 @@ import { SignupComponent } from './auth/signup/signup.component';
         BrowserModule,
         FormsModule,
         HttpModule,
-        ReactiveFormsModule
+        RecipeModule
     ],
     providers: [
         AuthGuard,
